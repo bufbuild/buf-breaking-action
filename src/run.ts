db5c67a74b4f47e46bf65cbad7819b46564883a3
Alex McKinney <alexmckinney01@gmail.com>
2021-03-18 10:31:24 -0700

    Refactor with eslint

--- src/run.ts
+++ src/run.ts
@@ -5,65 +5,67 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github'
 import * as io from '@actions/io';
-import * as path from "path";
-import cp from 'child_process';
 import { breaking } from './buf';
 import { Error, isError } from './error';
 import { postComments } from './github';
 
-export async function run() {
+export async function run(): Promise<void> {
     try {
-        const result = await runBreaking()
-        if (isError(result)) {
-            core.setFailed(result.errorMessage);
+        const result = await runBreaking();
+        if (result != null && isError(result)) {
+            core.setFailed(result.message);
         }
     } catch (error) {
         // In case we ever fail to catch an error
         // in the call chain, we catch the error
         // and mark the build as a failure. The
         // user is otherwise prone to false positives.
-        core.setFailed(error.message);
+        if (isError(error)) {
+            core.setFailed(error.message);
+            return;
+        }
+        core.setFailed('Internal error');
     }
 }
 
 // runBreaking runs the buf-breaking action, and returns
 // a non-empty error if it fails.
-async function runBreaking(): Promise<void|Error> {
+async function runBreaking(): Promise<null|Error> {
     const authenticationToken = core.getInput('github_token');
     if (authenticationToken === '') {
         return {
-            errorMessage: 'a Github authentication token was not provided'
+            message: 'a Github authentication token was not provided'
         };
     }
     const input = core.getInput('input');
     if (input === '') {
         return {
-            errorMessage: 'an input was not provided'
+            message: 'an input was not provided'
         };
     }
     const against = core.getInput('against');
     if (against === '') {
         return {
-            errorMessage: 'an against was not provided'
+            message: 'an against was not provided'
         };
     }
     const owner = github.context.repo.owner;
     if (owner === '') {
         return {
-            errorMessage: 'an owner was not provided'
+            message: 'an owner was not provided'
         };
     }
     const repository = github.context.repo.repo;
     if (repository === '') {
         return {
-            errorMessage: 'a repository was not provided'
+            message: 'a repository was not provided'
         };
     }
     const binaryPath = await io.which('buf', true);
     if (binaryPath === '') {
         // TODO: Update this reference to a link once it's available.
         return {
-            errorMessage: 'buf is not installed; please add the "bufbuild/setup-buf" step to your job'
+            message: 'buf is not installed; please add the "bufbuild/setup-buf" step to your job'
         };
     }
 
@@ -73,7 +75,7 @@ async function runBreaking(): Promise<void|Error> {
     }
     if (result.fileAnnotations.length === 0) {
         core.info('No breaking errors were found.');
-        return;
+        return null;
     }
 
     const pullRequestNumber = github.context.payload.pull_request?.number;
@@ -91,12 +93,16 @@ async function runBreaking(): Promise<void|Error> {
         } catch (error) {
             // Log the error, but continue so that we still write
             // out the raw output to the user.
-            core.info(`Failed to write comments in-line: ${error}`);
+            if (isError(error)) {
+                core.info(`Failed to write comments in-line: ${error.message}`);
+            } else {
+                core.info(`Failed to write comments in-line`);
+            }
         }
     }
 
     // Include the raw output so that the console includes sufficient context.
     return {
-        errorMessage: `buf found ${result.fileAnnotations.length} breaking failures.\n${result.raw}`
+        message: `buf found ${result.fileAnnotations.length} breaking failures.\n${result.raw}`
     };
 }
