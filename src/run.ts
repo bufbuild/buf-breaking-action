831c01a1b90a12053bd4bb1c0fd9b6808f8876cb
Jumpei Matsuda <jmatsu.drm+github@gmail.com>
2022-06-24 03:38:09 +0900

    feat: set the generated file annotations as one of outputs (#21)

--- src/run.ts
+++ src/run.ts
@@ -130,6 +130,9 @@ async function runBreaking(): Promise<null|Error> {
     if (isError(result)) {
         return result
     }
+
+    core.setOutput('results', result.fileAnnotations)
+
     if (result.fileAnnotations.length === 0) {
         core.info('No breaking errors were found.');
         return null;
